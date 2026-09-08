0d7e4050a354907049e340846242eff70d068859
gil-liGrota <gilgrota@gmail.com>
2026-09-08 15:57:04 +0300

    fix something

--- game_field.py
+++ game_field.py
@@ -23,6 +23,9 @@ def add_boom_grid():
     for i in range(20):
         row = random.randint(0, consts.BOARD_ROWS - 2)
         col =  random.randint(0, consts.BOARD_COLS)
+        while grid[row][col] == consts.CELL_STATE[0] or grid[row][col + 1] == consts.CELL_STATE[0] or grid[row][col + 2] == consts.CELL_STATE[0]:
+            row = random.randint(0, consts.BOARD_ROWS - 2)
+            col = random.randint(0, consts.BOARD_COLS)
         boom_lst.append((row, col))
 
         grid[row][col] = consts.CELL_STATE[1]
@@ -37,13 +40,7 @@ def random_x_y():
         lst = [(random.randint(0,consts.WINDOW_WIDTH-40),random.randint(0,consts.WINDOW_HEIGHT-40))]
     return lst
 
-def print_lst(lst):
-    for row in lst:
-        for col in row:
-            print(str(col), end=" ")
-        print()
 
 grid = board()
 get_flag_pos()
 add_boom_grid()
-print_lst(grid)
