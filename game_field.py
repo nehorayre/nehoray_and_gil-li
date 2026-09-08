336ef1403855472a304321ddd92f57b2e974aa73
nehoray revah <naamalivnee@gmail.com>
2026-09-08 14:48:40 +0300

    create EMPTY board

--- game_field.py
+++ game_field.py
@@ -1,10 +1,15 @@
 import consts
-
+import random
 def board():
     grid_game = []
     for row in range(consts.BOARD_ROWS):
         for col in range(consts.BOARD_COLS):
             grid_game.append(consts.CELL_STATE[0])
-
     return grid_game
 print(board())
+
+def random_grass_place_x_y():
+    grass_list = []
+    for i in range(20):
+        grass_list = [(random.randint(0,consts.WINDOW_WIDTH-40),random.randint(0,consts.WINDOW_HEIGHT-40))]
+    return grass_list
