3b36b8d925d2a20d03500619413ce40f4748a664
gil-liGrota <gilgrota@gmail.com>
2026-09-08 13:37:41 +0300

    finish the soldier

--- game_field.py
+++ game_field.py
@@ -8,3 +8,11 @@ def board():
 
     return grid_game
 print(board())
+
+def get_flag_pos():
+    flag_pos = []
+    for row in range(3):
+        for col in range(4):
+            flag_pos.append((consts.flag_row + row, consts.flag_col + col))
+    return flag_pos
+print(get_flag_pos())
--- ‪​soldier.py
+++ ‪​soldier.py
@@ -1,4 +1,5 @@
 import consts
+import game_field
 
 def init_soldier():
     return {
@@ -20,12 +21,22 @@ def is_touching_boom(boom_pos):
             return True
     return False
 
-def is_touching_flag(flag_pos):
+def is_touching_flag():
+    flag_pos = game_field.get_flag_pos()
     for body in range(len(soldier_legs)):
         if soldier_legs[body] in flag_pos:
             return True
     return False
 
+def is_inside_border():
+    for body in range(len(soldier_full_body)):
+        if soldier_full_body[body][0] < 0 or soldier_full_body[body][0] > consts.BOARD_ROWS:
+            if soldier_full_body[body][1] < 0 or soldier_full_body[body][1] > consts.BOARD_COLS:
+                return False
+    return True
+
+
 soldier = init_soldier()
 soldier_legs = get_soldier_leg_pos(soldier["left_up_x"], soldier["left_up_y"])
 soldier_body = get_soldier_body(soldier["left_up_x"], soldier["left_up_y"])
+soldier_full_body = soldier_body + soldier_legs
